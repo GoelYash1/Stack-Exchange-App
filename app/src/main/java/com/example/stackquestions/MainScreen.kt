9b197a532dab0b8cfca11a78f1853787c38d8549
Yash Goel <89704419+GoelYash1@users.noreply.github.com>
2023-05-13 16:52:41 +0530

    The UI is almost ready

--- app/src/main/java/com/example/stackquestions/MainScreen.kt
+++ app/src/main/java/com/example/stackquestions/MainScreen.kt
@@ -1,18 +1,72 @@
 package com.example.stackquestions
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
+import androidx.constraintlayout.solver.widgets.Helper
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.stackquestions.models.Question
+import com.example.stackquestions.util.HelperFunctions
 import com.example.stackquestions.util.Resource
 import com.example.stackquestions.viewmodels.QuestionViewModel
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.concurrent.TimeUnit
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun MainScreen(
     viewModel: QuestionViewModel
@@ -42,11 +96,140 @@ fun MainScreen(
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun DisplayQuestionItemUI(
     question: Question
 ) {
-    Column {
-        Text(text = question.title)
+    val minutesAgo = HelperFunctions.getTimeAgo(question.creation_date.toLong())
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .border(2.dp, Color.Black)
+            .shadow(10.dp, RoundedCornerShape(10.dp))
+            .fillMaxWidth(),
+        content = {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .padding(start = 8.dp, end = 8.dp, top = 8.dp, bottom = 24.dp)
+                    .fillMaxWidth()
+                ,
+                content = {
+                    Column(
+                        verticalArrangement = Arrangement.Top,
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(10.dp))
+                            .fillMaxWidth(0.2f)
+                    ) {
+                        LoadImageFromUrl(imageUrl = question.owner.profile_image)
+                    }
+                    Column(
+                        Modifier
+                            .fillMaxHeight()
+                            .absoluteOffset(x = (-30).dp, y = 10.dp)
+                            .border(1.dp, Color.Black, RoundedCornerShape(10.dp))
+                            .padding(
+                                start = 40.dp,
+                                end = 5.dp,
+                                top = 30.dp
+                            )
+                            .zIndex(-1f),
+                        content = {
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Text(
+                                    text = question.owner.display_name,
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    modifier = Modifier.padding(bottom = 4.dp),
+                                    overflow = TextOverflow.Ellipsis
+                                )
+                                Icon(
+                                    imageVector = Icons.Default.Favorite,
+                                    contentDescription = "Add to Favourites",
+                                    tint = MaterialTheme.colorScheme.onBackground,
+                                    modifier = Modifier.size(20.dp)
+                                )
+                            }
+                            Text(
+                                text = "-$minutesAgo",
+                                fontSize = 12.sp,
+                                color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.8f)
+                            )
+                            Text(
+                                text = question.title,
+                                fontSize = 16.sp,
+                                lineHeight = 20.sp,
+                                modifier = Modifier.padding(bottom = 8.dp)
+                            )
+                            LazyRow(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                                contentPadding = PaddingValues(vertical = 4.dp)
+                            ) {
+                                items(question.tags) { tag ->
+                                    Text(
+                                        text = "#$tag",
+                                        color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.8f),
+                                        fontSize = 12.sp
+                                    )
+                                }
+                            }
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                modifier = Modifier.fillMaxWidth().padding(bottom = 4.dp)
+                            ) {
+                                Text(
+                                    text = "${question.score} votes",
+                                    fontSize = 14.sp,
+                                    fontWeight = FontWeight.Medium,
+                                )
+                                Text(text = "${question.answer_count} answers",
+                                    fontSize = 14.sp,
+                                    fontWeight = FontWeight.Medium,
+                                )
+                                Text(text = "${question.view_count} views",
+                                    fontSize = 14.sp,
+                                    fontWeight = FontWeight.Medium,
+                                )
+                            }
+                        }
+                    )
+                }
+            )
+        }
+    )
+}
+
+@Composable
+fun LoadImageFromUrl(imageUrl: String) {
+    val context = LocalContext.current
+    var image: ImageBitmap? by remember(imageUrl) {
+        mutableStateOf(null)
+    }
+
+    Glide.with(context)
+        .asBitmap()
+        .load(imageUrl)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                image = resource.asImageBitmap()
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {}
+        })
+
+    image?.let {
+        Image(
+            bitmap = it,
+            contentDescription = "Image loaded from URL",
+            modifier = Modifier
+                .clip(RoundedCornerShape(10.dp))
+                .fillMaxSize()
+        )
     }
 }
+
